ea9ba343b83a320358f7ef54b94e63af41f31cfb
Gustavo Scharf <scharfgustavo@gmail.com>
2022-05-12 17:22:02 -0400

    user can now view their weekly production on main page

--- public/scripts/viewJobs.js
+++ public/scripts/viewJobs.js
@@ -10,8 +10,119 @@ function logoutRedirect() {
   window.location.replace("http://192.168.86.36:3000/");
 }
 
+function parseJumbledJSON(txt) {
+  return JSON.parse(txt.replace(/&quot;/g, '"'));
+}
 
+function clearTable(tableId, loggedInCrew) {
+  let table = document.getElementById(tableId);
+  let rows = table.querySelectorAll('tr');
 
+  for (let i = 1; i < rows.length; i++) {
+    let cells = rows[i].querySelectorAll('td');
+    if (cells[1].textContent !== loggedInCrew) {
+      rows[i].style.display = "none";
+    }
+    if (!thisWeek(cells[4].textContent)) {
+      rows[i].style.display = "none";
+    }
+  }
+}
 
+function calculateWeeklyTotals() {
+  let boreTable = document.getElementById('weeklyBoresTable');
+  let rows = boreTable.querySelectorAll('tr');
+  let boreTotal = 0;
+
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i].style.display !== "none") {
+      let cells = rows[i].querySelectorAll('td');
+      let footage = Number(cells[0].textContent.slice(0, -2));
+      boreTotal += footage;
+    }
+  }
+
+  let boreHeader = document.getElementById('weeklyBoresHeader');
+  boreHeader.textContent += ` ${boreTotal}ft`;
+
+  let rockTable = document.getElementById('weeklyRocksTable');
+  rows = rockTable.querySelectorAll('tr');
+  let rockTotal = 0;
+
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i].style.display !== "none") {
+      let cells = rows[i].querySelectorAll('td');
+      let footage = Number(cells[0].textContent.slice(0, -2));
+      rockTotal += footage;
+    }
+  }
+
+  let rocksHeader = document.getElementById('weeklyRocksHeader');
+  rocksHeader.textContent += ` ${rockTotal}ft`;
+
+  let vaultTable = document.getElementById('weeklyVaultsTable');
+  rows = vaultTable.querySelectorAll('tr');
+  let vaultTotals = {
+    "DT20": 0,
+    "DT30": 0,
+    "DT36": 0,
+  };
+
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i].style.display !== "none") {
+      let cells = rows[i].querySelectorAll('td');
+      vaultTotals[cells[0].textContent] += 1;
+    }
+  }
+
+  let totalVaults = vaultTotals["DT20"] + vaultTotals["DT30"] + vaultTotals["DT36"];
+
+  let vaultsHeader = document.getElementById('weeklyVaultsHeader');
+  vaultsHeader.textContent += ` ${vaultTotals["DT20"]} / ${vaultTotals["DT30"]} / ${vaultTotals["DT36"]} - ${totalVaults}`
+}
+
+function clearData() {
+  // clears wrong crew and not this week
+  let loggedInCrew = document.cookie.split("=")[1];
+  console.log(`loggedInCrew: ${loggedInCrew}`);
+
+  let tables = ["weeklyBoresTable", "weeklyVaultsTable", "weeklyRocksTable"];
+  for (const table of tables) {
+    clearTable(table, loggedInCrew);
+  }
+
+}
+
+function getMonday() {
+  let currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
+  let originalWeekday = currentDate.getDay();
+  let currentDay = currentDate.getDate();
+  let currentWeekday = currentDate.getDay();
+
+  let delta = currentWeekday - 1;
+  currentDate.setDate(currentDay - delta);
+  if (originalWeekday == 0) {
+    currentDate.setDate(currentDate.getDate() - 7);
+  }
+  return currentDate;
+}
+
+function thisWeek(dateStr) {
+  let monday = getMonday();
+  let mondayUTC = monday.valueOf();
+  monday.setDate(monday.getDate() + 6);
+  let saturdayUTC = monday.valueOf();
+
+  let itemDate = new Date(dateStr);
+  let itemUTC = itemDate.valueOf();
+  if (itemUTC >= mondayUTC && itemUTC <= saturdayUTC) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 setUsernameHeader();
+clearData();
+calculateWeeklyTotals();
--- routes/viewJobs.js
+++ routes/viewJobs.js
@@ -2,6 +2,25 @@ const express = require('express');
 const router = express.Router();
 const { pool } = require('../db');
 
+function formatDate(dateStr) {
+  let date = new Date(dateStr);
+  let weekday = date.getDay();
+  let day = String(date.getDate()).padStart(2, "0");
+  let month = String(date.getMonth() + 1).padStart(2, "0");
+  let year = date.getFullYear();
+
+  let dayTrans = {
+    0: "Sun",
+    1: "Mon",
+    2: "Tue",
+    3: "Wed",
+    4: "Thu",
+    5: "Fri",
+    6: "Sat",
+  };
+  return `${dayTrans[weekday]} ${month}/${day}/${year}`;
+}
+
 router.get('/', (req, res, next) => {
   pool.query('SELECT * FROM pages', (err, resp) => {
     if (err) {
@@ -27,8 +46,49 @@ router.get('/', (req, res, next) => {
       };
       parsedJobs.push(obj);
     }
+    pool.query(`SELECT bores.id, bores.footage, bores.job_name, bores.crew_name, bores.page_id, bores.work_date, pages.page_number FROM bores INNER JOIN pages on bores.page_id=pages.id`, (err, resp2) => {
+      if (err) {
+        console.log(`error pulling bores for viewJobs`);
+      }
+      let bores = resp2.rows;
+      for (const bore of bores) {
+        bore.work_date_formatted = formatDate(bore.work_date);
+      }
+      pool.query(`SELECT vaults.id, vaults.vault_size, vaults.crew_name, vaults.job_name, vaults.work_date, vaults.page_id, pages.id FROM vaults INNER JOIN pages ON vaults.page_id=pages.id`, (err, resp3) => {
+        if (err) {
+          console.log(`error pulling vaults for viewJobs`);
+        }
 
-    res.render('viewJobs', { jobs: parsedJobs });
+        let vaultTrans = {
+          0: "DT20",
+          1: "DT30",
+          2: "DT36",
+        };
+
+        let vaults = resp3.rows;
+        for (const vault of vaults) {
+          vault.work_date_formatted = formatDate(vault.work_date);
+          vault.vault_size_formatted = vaultTrans[vault.vault_size];
+        }
+        pool.query(`SELECT rocks.id, rocks.footage, rocks.job_name, rocks.crew_name, rocks.page_id, rocks.work_date, pages.page_number FROM rocks INNER JOIN pages on rocks.page_id=pages.id`, (err, resp4) => {
+          if (err) {
+            console.log(`error pulling rocks for viewJobs`);
+          }
+          let rocks = resp4.rows;
+          for (const rock of rocks) {
+            rock.work_date_formatted = formatDate(rock.work_date);
+          }
+
+
+          res.render('viewJobs', {
+            jobs: parsedJobs,
+            bores: bores,
+            rocks: rocks,
+            vaults: vaults,
+          });
+        });
+      });
+    });
   });
 });
 
