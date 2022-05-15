7a7de37f0f2dca91e302390ab68ef596ecbba25d
Gustavo Scharf <scharfgustavo@gmail.com>
2022-05-15 16:55:08 -0400

    fixed bug that showed bores related to a job on all pages because im bad at sql

--- routes/input.js
+++ routes/input.js
@@ -14,7 +14,7 @@ router.get('/:jobName/:pageId', (req, res, next) => {
 
     pool.query(
       `SELECT bores.id, bores.footage, bores.crew_name, bores.job_name, bores.page_id, bores.position, bores.work_date, pages.page_number FROM bores
-      INNER JOIN pages ON bores.job_name=pages.job_name
+      INNER JOIN pages ON bores.job_name=pages.job_name AND bores.page_id=pages.id
       WHERE pages.page_number=${pageId}
       AND bores.job_name='${jobName}';`
       , (err, resp2) => {
@@ -22,14 +22,14 @@ router.get('/:jobName/:pageId', (req, res, next) => {
         let bores = resp2.rows;
         pool.query(
           `SELECT vaults.id, vaults.crew_name, vaults.vault_size, vaults.job_name, vaults.page_id, vaults.position, vaults.work_date, pages.page_number FROM vaults
-          INNER JOIN pages on vaults.job_name=pages.job_name
+          INNER JOIN pages on vaults.job_name=pages.job_name AND vaults.page_id=pages.id
           WHERE pages.page_number=${pageId}
           AND vaults.job_name='${jobName}';`
           , (err, resp3) => {
             let vaults = resp3.rows;
             pool.query(
               `SELECT rocks.id, rocks.footage, rocks.crew_name, rocks.job_name, rocks.page_id, rocks.position, rocks.work_date, pages.page_number FROM rocks
-              INNER JOIN pages ON rocks.job_name=pages.job_name
+              INNER JOIN pages ON rocks.job_name=pages.job_name AND rocks.page_id=pages.id
               WHERE pages.page_number=${pageId}
               AND rocks.job_name='${jobName}';`
               , (err, resp4) => {
